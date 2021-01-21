12292b5a1f98f844082c014542ba7b2127439e8c
Jeff Bolduan <jbolduan@gmail.com>
2021-01-21 12:48:16 -0600

    Added additional tests to cover more possibilities

--- tests/Compare-Version.Tests.ps1
+++ tests/Compare-Version.Tests.ps1
@@ -23,6 +23,23 @@ try {
 
             It "[System.Version] Should return true if the reference version is larger than the difference version." {
                 Compare-Version -ReferenceVersion "2.0.0.0" -DifferenceVersion "1.0.0.0" | Should -BeTrue
+                Compare-Version -ReferenceVersion "2.0.0" -DifferenceVersion "1.0.0.0" | Should -BeTrue
+            }
+
+            It "[SemVer] Should return false if the reference version is smaller than the difference version." {
+                Compare-Version -ReferenceVersion "1.0.0" -DifferenceVersion "2.0.0" | Should -BeFalse
+                Compare-Version -ReferenceVersion "2.0.0-alpha1" -DifferenceVersion "2.0.0-beta1" | Should -BeFalse
+                Compare-Version -ReferenceVersion "20" -DifferenceVersion "36" | Should -BeFalse
+            }
+
+            It "[System.Version] Should return false if the reference version is smaller than the difference version." {
+                Compare-Version -ReferenceVersion "1.0.0.0" -DifferenceVersion "2.0.0.0" | Should -BeFalse
+                Compare-Version -ReferenceVersion "1.0.0" -DifferenceVersion "2.0.0.0" | Should -BeFalse
+            }
+
+            It "Should throw if the version is not parsable by the current logic of the function" {
+                { Compare-Version -ReferenceVersion "alpha1" -DifferenceVersion "beta1" } | Should -Throw
+                { Compare-Version -ReferenceVersion "1.0.0-alpha1" -DifferenceVersion "1.0.0.0" } | Should -Throw
             }
         }
     }
