b8fafe7f94eb677b8cddd0f7f5f0c583deca5ea1
Jeff Bolduan <jbolduan@gmail.com>
2021-01-21 13:08:55 -0600

    Added comment based help

--- UMN-AutoPackager/public/Compare-Version.ps1
+++ UMN-AutoPackager/public/Compare-Version.ps1
@@ -1,7 +1,30 @@
+<#
+    .SYNOPSIS
+        Takes a reference object and a difference object and determines if the reference object is greater than the difference object.
+    .DESCRIPTION
+        Takes a reference object and a difference object and determines if the reference object is greater than the difference object.
+
+        Example: (reference) 1.0 > 0.1 (difference) would return true
+    .EXAMPLE
+        Compare-Version -ReferenceVersion "1.0.0.0" -DifferenceVersion "2.0.0.0" would return $false
+    .EXAMPLE
+        Compare-Version -ReferenceVersion "2.0.0-beta1" -DifferenceVersion "2.0.0-alpha12" would return $true
+    .PARAMETER ReferenceVersion
+        Version as a string which is on the left side of the greater than equation.
+    .PARAMETER DifferenceVersion
+        Version as a string which is on the right side of the greater than equation.
+#>
 function Compare-Version {
     [CmdletBinding()]
     param(
+        [Parameter(Mandatory = $true,
+            HelpMessage = "Version as a string which is on the left side of the greater than equation.")]
+        [ValidateNotNullOrEmpty()]
         [string]$ReferenceVersion,
+
+        [Parameter(Mandatory = $true,
+            HelpMessage = "Version as a string which is on the right side of the greater than equation.")]
+        [ValidateNotNullOrEmpty()]
         [string]$DifferenceVersion
     )
     
