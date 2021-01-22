98fa1b69f5122cc729fbccfc766964b91b47b252
thoen008 <thoen008@umn.edu>
2021-01-22 15:14:09 -0600

    Getting all the names
    Working on getting all the various names in the JSON and expanding the ones that are in arrays.

--- UMN-AutoPackager/public/Set-Config.ps1
+++ UMN-AutoPackager/public/Set-Config.ps1
@@ -3,3 +3,42 @@
 # Set-Config -Path .\config.json -Object ConfigMgr -Name SiteCode  -Value "UMN"
 # Set-Config -Path .\config.json -Name Company  -Value "University of Minnesota"
 # (Get-CMSite).SiteCode | Set-Config -Path .\config.json -Object ConfigMgr  -Name SiteCode
+# $json = Get-Content C:\users\thoen008\Desktop\GlobalConfig.json -Raw
+# $obj = ConvertFrom-Json -InputObject $json
+function Set-Config {
+    [CmdletBinding()]
+    param (
+        [string]$Path,
+        [string]$Name,
+        [string]$Value
+    )
+    begin {
+    }
+    process {
+        $json = Get-Content $Path -Raw
+        $config = ConvertFrom-Json -InputObject $json
+        $Names = $config | Get-Member -MemberType Properties | Select-Object -ExpandProperty Name
+        foreach ($name in $Names) {
+            if ($config.$name -eq "PSCustomObject") {
+                $NewNames = $Config.$Name | Get-Member -MemberType Properties | Select-Object -ExpandProperty Name
+                foreach ($newname in $NewNames) {
+                    # check against the $name and update the value with the new one
+                }
+            }
+            elseif ($config.$name -eq "String") {
+                # Check against the $name and update the value with the new one
+            }
+        }
+    }
+    end {
+    }
+}
+
+<# [PSCustomObject]@{
+    CompanyName = "MyCompany"
+    LastModified = (Get-Date).ToString()
+    ConfigMgr = @{Site = "site.something.com"
+                 SiteCode = "COM"}
+    RecipeLocations = @{Location1 = "C:\Temp"
+                     Location2 = "\\files.somewhere\"}
+} | convertto-json | Set-Content C:\Users\thoen008\Desktop\GlobalConfig.json #>
