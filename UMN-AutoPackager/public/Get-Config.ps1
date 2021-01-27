63ba0328702571719fe3578f553625a579608438
thoen008 <thoen008@umn.edu>
2021-01-27 09:22:55 -0600

    Get-Config initial working function
    Got the function working. Need to add help and more function elements.

--- UMN-AutoPackager/public/Get-Config.ps1
+++ UMN-AutoPackager/public/Get-Config.ps1
@@ -1,4 +1,3 @@
-# Pulls a json config file from a standard location. Gets global configurations for the auto packager out of the json config file. This will need to be determined when building get/set config functions.
 function Get-Config {
     [CmdletBinding()]
     param (
@@ -9,19 +8,9 @@ function Get-Config {
     process {
         $json = Get-Content $Path -Raw
         $config = ConvertFrom-Json -InputObject $json
-        $Names = $config | Get-Member -MemberType Properties | Select-Object -ExpandProperty Name
-        foreach ($name in $Names) {
-            if ($config.$name -eq "PSCustomObject") {
-                $NewNames = $Config.$Name | Get-Member -MemberType Properties | Select-Object -ExpandProperty Name
-                foreach ($newname in $NewNames) {
-                    # Output the name and value(s)
-                }
-            }
-            elseif ($config.$name -eq "String") {
-                # Output the value
-            }
+        Write-Output $config
         }
-    }
     end {
     }
 }
+$test = Get-Config -path C:\Users\thoen008\Desktop\GlobalConfig.json
