24f60e4feeea4d427bc7317b3a36da1396047512
thoen008 <thoen008@umn.edu>
2021-02-01 11:28:15 -0600

    Updated the function and JSON based on feedback
    Updated the JSON with some new arrays for ConfigMgr/RecipeLocations. Updated the function set-umnglobalConfig.ps1 with changes to accomadate the new arrays in the json. Commented out some of the Write-Verbose. Updated the help with additional examples using the additional Parameter sets needed.

--- UMN-AutoPackager/public/Set-UMNGlobalConfig.ps1
+++ UMN-AutoPackager/public/Set-UMNGlobalConfig.ps1
@@ -1,33 +1,3 @@
-function Get-UMNGlobalConfig {
-    <#
-    .SYNOPSIS
-        Gets all the global configurations and value for each from a JSON file used by the auto packager.
-    .DESCRIPTION
-        This command retrieves from a json file the values of various global configurations used as part of the AutoPackager. It requires a the full path and name of the JSON file.
-    .EXAMPLE
-        Get-UMNGlobalConfig -Path .\config.json
-        Gets the values of the config.json file
-    .PARAMETER Path
-        The full path and file name of the JSON file to get the config from
-    #>
-    [CmdletBinding()]
-    param (
-        [Parameter(Mandatory=$True, HelpMessage = "The full path and file name of the JSON file to get the config from")]
-        [ValidateNotNullOrEmpty()]
-        [string]$Path
-    )
-    begin {
-        Write-Verbose -Message "Starting $($myinvocation.mycommand)"
-    }
-    process {
-        $json = Get-Content -Path $Path -Raw
-        $config = ConvertFrom-Json -InputObject $json
-        Write-Output -InputObject $config
-        }
-    end {
-        Write-Verbose -Message "Ending $($myinvocation.mycommand)"
-    }
-}#Get-Config
 function Set-UMNGlobalConfig {
     <#
     .SYNOPSIS
@@ -35,17 +5,20 @@ function Set-UMNGlobalConfig {
     .DESCRIPTION
         This command sets in a json file a value for one of various global configurations used as part of the AutoPackager. It requires a the full path and name of the JSON file, the key to set, and the value to set it to.
     .EXAMPLE
-        Set-UMNGlobalConfig -Path .\config.json -Key CompanyName  -Value "University of Minnesota"
+        Set-UMNGlobalConfig -Path .\GlobalConfig.json -Key CompanyName  -Value "University of Minnesota"
         Sets the value of CompanyName to University of Minnesota for the config.json file
     .EXAMPLE
-        Set-UMNGlobalConfig -Path .\config.json -SiteServer "my.config.site"  -SiteCode "COM" -DownloadLocationPath "C:\\Temp" -ApplicatoinContentPath "\\appstorage.somewhere\"
+        Set-UMNGlobalConfig -Path .\GlobalConfig.json -Key ConfigMgr -SiteServer "my.config.site"  -SiteCode "COM" -DownloadLocationPath "C:\\Temp" -ApplicationContentPath "\\appstorage.somewhere\"
         Sets the values for a ConfigMgr site to be added to the config.json file
+    .EXAMPLE
+        Set-UMNGlobalConfig -Path .\GlobalConfig.json -Key RecipeLocations -Value "\\file.server\recipes" -LocationType "directory"
+        Adds the location "\\files.server\recipes" and LocationType "directory" to the RecipeLocations key in the JSON specified
     .PARAMETER Path
         The full path and file name of the JSON file to be updated
     .PARAMETER Key
         The key that you want to update in the Global Configuation JSON file
     .PARAMETER Value
-        The value you want to set the key to
+        The single value you want to set the key to
     .PARAMETER SiteServer
         The ConfigMgr site server address
     .PARAMETER SiteCode
@@ -53,18 +26,24 @@ function Set-UMNGlobalConfig {
     .PARAMETER DownloadLocationPath
         The full path where downloaded data will be temporarily stored
     .PARAMETER ApplicationContentPath
-        The full path for where ConfigMgr applcation content will be stored"
+        The full path for where ConfigMgr applcation content will be stored
+    .PARAMETER LocationType
+        The location type for where the recipes will be stored for the autopackager. Directory will be the most common.
     #>
     [CmdletBinding(DefaultParameterSetName="Main")]
     param (
         [Parameter(ParameterSetName="Main",Mandatory=$True, HelpMessage = "The full path and file name of the JSON file to be updated")]
         [Parameter(ParameterSetName="ConfigMgr")]
+        [Parameter(ParameterSetName="RecipeLocations")]
         [ValidateNotNullOrEmpty()]
         [string]$Path,
         [Parameter(ParameterSetName="Main",Mandatory=$True, HelpMessage = "The key that you want to update in the Global Configuation JSON file")]
+        [Parameter(ParameterSetName="ConfigMgr")]
+        [Parameter(ParameterSetName="RecipeLocations")]
         [ValidateNotNullOrEmpty()]
         [string]$Key,
         [Parameter(ParameterSetName="Main",Mandatory=$True, HelpMessage = "The value you want to set the key to")]
+        [Parameter(ParameterSetName="RecipeLocations")]
         [ValidateNotNullOrEmpty()]
         [string]$Value,
         [Parameter(ParameterSetName="ConfigMgr",Mandatory=$True, HelpMessage = "The ConfigMgr site server address")]
@@ -74,10 +53,12 @@ function Set-UMNGlobalConfig {
         [Parameter(ParameterSetName="ConfigMgr",Mandatory=$True, HelpMessage = "The full path where downloaded data will be temporarily stored")]
         [string]$DownloadLocationPath,
         [Parameter(ParameterSetName="ConfigMgr",Mandatory=$True, HelpMessage = "The full path for where ConfigMgr applcation content will be stored")]
-        [string]$ApplicationContentPath
+        [string]$ApplicationContentPath,
+        [Parameter(ParameterSetName="RecipeLocations",Mandatory=$True, HelpMessage = "The location type for where the recipes will be stored for the autopackager.")]
+        [string]$LocationType
     )
     begin {
-        Write-Verbose -Message "Starting $($myinvocation.mycommand) Updating $key with $value"
+        Write-Verbose -Message "Starting $($myinvocation.mycommand)"
     }
     process {
         $config = Get-UMNGlobalConfig -Path $Path
@@ -87,7 +68,7 @@ function Set-UMNGlobalConfig {
             foreach ($k in $Keys) {
                 Write-Verbose -Message "Querying $k"
                 if ($config.$k.gettype().Name -eq 'String') {
-                    Write-Verbose -Message "$k is a string"
+                  # Write-Verbose -Message "$k is a string"
                     if ($k -eq $Key) {
                         Write-Verbose -Message "Found match $k setting new value"
                         $config.$k = $Value
@@ -100,29 +81,15 @@ function Set-UMNGlobalConfig {
                            Write-Warning -Message "Failed to update JSON file. $($_Exception.message)"
                         }
                     }
-                }
+                }#String Type
                 if ($config.$k.gettype().Name -eq "Object[]") {
-                   Write-Verbose -Message "$k is Array"
+                  # Write-Verbose -Message "$k is Array"
                    if ($PSCmdlet.ParameterSetName -eq "Main") {
-                       Write-Verbose -Message "Parameter set is Main"
-                       # add a parameter check for Configmgr
+                      # Write-Verbose -Message "Parameter set is Main"
                         if ($k -eq $Key) {
                             Write-Verbose -Message "Found match for $k"
                             if ($config.$k -contains $value) {
-                                # Move these tests to the specific if statements
-                                Write-Verbose -Message "Already contains $value"
-                            }
-                            elseif ($k -eq "RecipeLocations") {
-                                # Maybe make a new Parameter set for recipelocations and add locationType
-                                $config.$k += New-Object -TypeName PSObject -Property ([ordered]@{locationType="directory";locationUri=$Value})
-                                $config.LastModified = (Get-Date).ToString()
-                                try {
-                                    $config | convertto-json | Set-Content -Path $path
-                                Write-Verbose -Message "$key is updated with $value"
-                                }
-                                catch {
-                                Write-Warning -Message "Failed to update JSON file. $($_Exception.message)"
-                                }
+                                Write-Warning -Message "$Key already contains $value"
                             }
                             else {
                                 [System.collections.ArrayList]$new = $config.$k
@@ -139,15 +106,55 @@ function Set-UMNGlobalConfig {
                             }
                         }
                    }
+                   if ($PSCmdlet.ParameterSetName -eq "RecipeLocations") {
+                       # Write-Verbose -Message "Parameter set is RecipeLocations"
+                        if ($k -eq "RecipeLocations") {
+                            if ($config.$k.locationUri -contains $value) {
+                                Write-Warning -Message "$Key already contains $value"
+                            }
+                            else {
+                                $config.$k += New-Object -TypeName PSObject -Property ([ordered]@{locationType=$LocationType;locationUri=$Value})
+                                $config.LastModified = (Get-Date).ToString()
+                                try {
+                                    $config | convertto-json | Set-Content -Path $path
+                                Write-Verbose -Message "$key is updated with $value"
+                                }
+                                catch {
+                                Write-Warning -Message "Failed to update JSON file. $($_Exception.message)"
+                                }
+                            }
+                        }
+                    }
+                    if ($PSCmdlet.ParameterSetName -eq "ConfigMgr") {
+                        # Write-Verbose -Message "Parameter set is ConfigMgr"
+                        if ($k -eq "ConfigMgr") {
+                            if ($config.$k.Site -contains $SiteServer) {
+                                Write-Warning -Message "ConfigMgr already contains a Site Server: $SiteServer"
+                            }
+                            else {
+                                $config.$k += New-Object -TypeName PSObject -Property ([ordered]@{Site=$SiteServer;SiteCode=$SiteCode;DownloadLocationPath= $DownloadLocationPath;ApplicationContentPath=$ApplicationContentPath})
+                                $config.LastModified = (Get-Date).ToString()
+                                try {
+                                    $config | convertto-json | Set-Content -Path $path
+                                Write-Verbose -Message "$key is updated with $SiteServer information"
+                                }
+                                catch {
+                                Write-Warning -Message "Failed to update JSON file. $($_Exception.message)"
+                                }
+                            }
+                        }
+                    }
+                 }#Object{} Type
+                 else {
+                   # Write-Verbose -Message "Does not match $k"
                 }
-            } #foreach $key
+            }#foreach $key
         }
         else {
-            Write-Warning -Message "$Key is not a valid Key."
+            Write-Warning -Message "$Key is not a valid Key"
         }
     }
     end {
         Write-Verbose -Message "Ending $($myinvocation.mycommand)"
     }
 }#Set-Config
-Set-UMNGlobalConfig -Path C:\Users\thoen008\Desktop\GlobalConfig.json -Key "RecipeLocations" -Value "C:\temp2" -Verbose
