bc87a89f62441ddde1aab9ad442466d0cc549577
thoen008 <thoen008@umn.edu>
2021-02-01 15:25:56 -0600

    Added more comments on design
    Using comments intiially to figure out the design and what the function will do.

--- UMN-AutoPackager/public/Build-MEMCMPackage.ps1
+++ UMN-AutoPackager/public/Build-MEMCMPackage.ps1
@@ -1,1 +1,6 @@
 # Creates an MEMCM package based on the standard inputs from the package definition file
+# Build-MEMCMPackage -Path .\GlobalConfig.json -Name PackageConfig
+# Reads the globalconfig.json using get-UMNGlobalConfig for details on the various sites information. Retrieves the specified Pacakge Config Name using get-newpackagefile. Retrieves values of packagefile using Get-PackageDefition.
+# Foreach Site creates an application based on the values in the package config storing the content in the site's specified ApplicationContentPath.
+# Do we want this to accept multiple packageconfig names? Yes but build out a working function first
+# Do we want to expect from pipeline? Yes but first build out a working function first
--- UMN-AutoPackager/public/Deploy-MEMCMPackage.ps1
+++ UMN-AutoPackager/public/Deploy-MEMCMPackage.ps1
@@ -1,1 +1,11 @@
 # Creates standard deployments (may require definition in the global configuration) in MEMCM
+# Deploy-MEMCMPackage -Path GlobalConfig.json -Name PackageConfig
+# Creates a deployment(s) based on the data in the PackageConfig and GlobalConfig.
+# Needs to find the collection based on information in the packageconfig and globalconfig.
+# Needs to know what kind of deployment is selected. Maybe this is another reason to add it to the package config?
+# Need to know other deployment information maybe there is standard settings pulled out of the GlobalConfig or packageconfig.
+# How many days before deadline? Yes No Deadline? unique per application or global setting
+# Is this available or required? Maybe we assume all these will be required.
+# User experience setting? Alerts? Global Config or unique per application
+# Default Content distribution? Global Config
+# If we start using stuff besides single collection single deployment we will need to accomadate that.
