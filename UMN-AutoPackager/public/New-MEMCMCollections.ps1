039ac1a29c4f85e8532a15387626f36b675d442a
thoen008 <thoen008@umn.edu>
2021-05-04 14:18:45 -0500

    Added some logic for checking if the collection exists.

--- UMN-AutoPackager/public/New-MEMCMCollections.ps1
+++ UMN-AutoPackager/public/New-MEMCMCollections.ps1
@@ -12,7 +12,7 @@ function Get-UMNGlobalConfig {
     #>
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory=$True, HelpMessage = "The full path and file name of the JSON file to get the config from")]
+        [Parameter(Mandatory = $True, HelpMessage = "The full path and file name of the JSON file to get the config from")]
         [ValidateNotNullOrEmpty()]
         [string]$Path
     )
@@ -23,7 +23,7 @@ function Get-UMNGlobalConfig {
         $json = Get-Content -Path $Path -Raw
         $config = ConvertFrom-Json -InputObject $json
         Write-Output -InputObject $config
-        }
+    }
     end {
         Write-Verbose -Message "Ending $($myinvocation.mycommand)"
     }
@@ -65,14 +65,14 @@ function New-MEMCMCollections {
             foreach ($PkgObject in $PackageDefinition) {
                 Write-Verbose -Message "Processing the package definition $($pkgObject.publisher) $($pkgObject.productname)"
                 foreach ($collection in $PkgObject.CollectionTargets) {
-                    if ($collection.type -eq "MEMCM-Collection") {
+                    if (($collection.type -eq "MEMCM-Collection") -and (-not (Get-CMCollection -Name $collection.Name))) {
+                        # Collection does not exist, building the collection.
                         Write-Verbose -Message "Building collection $($collection.name) for ConfigMgr"
-                        # build in a check to see if the collection name exists if it does check the settings match? Or just skip.
                         $CollectionArguments = @{
-                            Name = $collection.Name
+                            Name                   = $collection.Name
                             LimitingCollectionName = $collection.LimitingCollectionName
-                            RefreshType = $collection.RefreshType
-                            RefreshSchedule = ""
+                            RefreshType            = $collection.RefreshType
+                            RefreshSchedule        = ""
                         }
                         # RefreshType is Periodic or Both. Build a schedule and create the collection.
                         if ($CollectionArguments.RefreshType -eq "Periodic" -or $CollectionArguments.RefreshType -eq "Both") {
@@ -81,30 +81,30 @@ function New-MEMCMCollections {
                             if ($collection.RecurInterval -eq "Days" -or $collection.RecurInterval -eq "Hours" -or $collection.RecurInterval -eq "Minutes") {
                                 Write-Verbose -Message "Periodic is using days, hours, or minutes"
                                 $sched = New-CMSchedule -Start $startdate -RecurInterval $collection.RecurInterval -Recurcount $collection.RecurCount
-                                $CollectionArguments.set_item("RefreshSchedule",$sched)
+                                $CollectionArguments.set_item("RefreshSchedule", $sched)
                             }
                             elseif ($collection.RecurInterval -eq "Month") {
                                 Write-Verbose -Message "Periodic using month"
                                 if ($collection.LastDayofMonth -eq $true) {
                                     Write-Verbose -Message "Periodic is using LastDayOfMonth"
                                     $sched = New-CMSchedule -Start $startdate -LastDayOfMonth
-                                    $CollectionArguments.set_item("RefreshSchedule",$sched)
+                                    $CollectionArguments.set_item("RefreshSchedule", $sched)
                                 }
                                 elseif ($collection.WeekOrder) {
                                     Write-Verbose -Message "Periodic is using WeekOrder"
                                     $sched = New-CMSchedule -Start $startdate -DayOfWeek $collection.DayOfWeek -WeekOrder $collection.WeekOrder -RecurCount $collection.RecurCount
-                                    $CollectionArguments.set_item("RefreshSchedule",$sched)
+                                    $CollectionArguments.set_item("RefreshSchedule", $sched)
                                 }
                                 elseif ($collection.DayOfMonth) {
                                     Write-Verbose -Message "Periodic is using Day of Month"
                                     $sched = New-CMSchedule -Start $startdate -DayOfMonth $collection.DayOfMonth -RecurCount $collection.RecurCount
-                                    $CollectionArguments.set_item("RefreshSchedule",$sched)
+                                    $CollectionArguments.set_item("RefreshSchedule", $sched)
                                 }
                             }
                             elseif ($collection.RecurInterval -eq "Week") {
                                 Write-Verbose -Message "Periodic is using week"
                                 $sched = New-CMSchedule -Start $startdate -DayOfWeek $collection.DayOfWeek -RecurCount $collection.RecurCount
-                                $CollectionArguments.set_item("RefreshSchedule",$sched)
+                                $CollectionArguments.set_item("RefreshSchedule", $sched)
                             }
                             # Create the periodic collection
                             Write-Verbose -Message "Creating the collection: $($CollectionArguments.Name)"
@@ -117,6 +117,7 @@ function New-MEMCMCollections {
                             }
                         }
                         else {
+                            # Create the non-periodic collection
                             Write-Verbose -Message "Not periodic creating the collection: $($CollectionArguments.Name)"
                             $CollectionArguments.Remove('RefreshSchedule')
                             try {
@@ -127,6 +128,9 @@ function New-MEMCMCollections {
                                 Write-Warning -Message "Error: $($_.Exception.Message)"
                             }
                         }
+                    }
+                    else {
+                        Write-Verbose -Message "$($collection.Name) exists already or is not a MEMCM-Collection type."
                     }
                 }#foreach $CollectionTargets
             }#foreach $PackageDefinition
