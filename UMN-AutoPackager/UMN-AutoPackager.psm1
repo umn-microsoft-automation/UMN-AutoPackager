b44428ab784e44bd40d030aef0f9414731c5e681
Jeff Bolduan <jbolduan@gmail.com>
2021-05-14 10:51:32 -0500

    Added check for $GlobalConfig and added standard location.

--- UMN-AutoPackager/UMN-AutoPackager.psm1
+++ UMN-AutoPackager/UMN-AutoPackager.psm1
@@ -1,6 +1,10 @@
 $Public = @( Get-ChildItem -Path $PSScriptRoot\public\*.ps1 -ErrorAction SilentlyContinue )
 $Private = @( Get-ChildItem -Path $PSScriptRoot\private\*.ps1 -ErrorAction SilentlyContinue )
 
+# Global Config Path
+$GlobalConfigPath = "$($env:ProgramData)\UMN\AutoPackager\GlobalConfig.json"
+Export-ModuleMember -Variable GlobalConfigPath
+
 foreach ($Import in @($Public + $Private)) {
     try {
         . $Import.FullName
@@ -8,6 +12,15 @@ foreach ($Import in @($Public + $Private)) {
     catch {
         Write-Error -Message "Failed to import function $($Import.FullName): $_"
     }
+}
+
+if (Test-Path -Path $GlobalConfigPath) {
+    $GlobalConfig = Get-UMNGlobalConfig -Path $GlobalConfigPath
+    
+    Export-ModuleMember -Variable GlobalConfig
+}
+else {
+    Write-Error -Message "Could not find a global config at $GlobalConfigPath.`nRun Set-UMNGlobalConfig using the `$GlobalConfigPath variable."
 }
 
 # Export only the powershell functions that are public
