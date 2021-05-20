9049ca2eccf590a007ed43f1515b1e7f29dc2774
Jeff Bolduan <jbolduan@gmail.com>
2021-05-19 20:48:54 -0500

    Fixed bug with files not getting removed

--- build/quick_build_local.ps1
+++ build/quick_build_local.ps1
@@ -1,6 +1,11 @@
 $Module = "UMN-AutoPackager"
 
 Push-Location $PSScriptRoot
+
+# Clear Output
+Get-ChildItem -Path "$PSScriptRoot\..\output\$Module" | Remove-Item -Force -Recurse -Confirm:$false
+
+# Build Module to Output
 dotnet build $PSScriptRoot\..\src -o $PSScriptRoot\..\output\$Module\bin
 Copy-Item "$PSScriptRoot\..\$Module\*" "$PSScriptRoot\..\output\$Module" -Recurse -Force
 
