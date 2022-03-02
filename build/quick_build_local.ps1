1710327d21b8731b789584a67f66ce421aa1b848
Jeff Bolduan <jbolduan@umn.edu>
2022-03-02 14:34:07 -0600

    enhance the quick build

--- build/quick_build_local.ps1
+++ build/quick_build_local.ps1
@@ -1,3 +1,8 @@
+param(
+    [switch]$PackageApp,
+    [switch]$CreateCollections,
+    [switch]$DeployApp
+)
 $Module = "UMN-AutoPackager"
 
 Push-Location $PSScriptRoot
@@ -6,11 +11,29 @@ Push-Location $PSScriptRoot
 Get-ChildItem -Path "$PSScriptRoot\..\output\$Module" | Remove-Item -Force -Recurse -Confirm:$false
 
 # Build Module to Output
-dotnet build $PSScriptRoot\..\src -o $PSScriptRoot\..\output\$Module\bin
+dotnet publish $PSScriptRoot\..\src -o $PSScriptRoot\..\output\$Module\bin
 Copy-Item "$PSScriptRoot\..\$Module\*" "$PSScriptRoot\..\output\$Module" -Recurse -Force
 
+$ModulePSD1 = Get-Content -Path "$PSScriptRoot\..\output\$Module\$Module.psd1"
+
+$UpdatedModule = $ModulePSD1.Replace("{version}", "0.0.1").Replace("'{prerelease}'", "''")
+
+Set-Content -Path "$PSScriptRoot\..\output\$Module\$Module.psd1" -Value $UpdatedModule
+
 $ArgList = @(
-    "-NoExit -Command ipmo .\UMN-AutoPackager.psd1"
+    "-NoExit -Command ipmo .\UMN-AutoPackager.psd1; Invoke-AutoPackager -GlobalConfig `$GlobalConfig -InformationAction Continue"
 )
+
+if ($PackageApp) {
+    $ArgList[0] = $ArgList[0] + " -PackageApp"
+}
+
+if ($CreateCollections) {
+    $ArgList[0] = $ArgList[0] + " -CreateCollections"
+}
+
+if ($DeployApp) {
+    $ArgList[0] = $ArgList[0] + " -DeployApp"
+}
 
 Start-Process -FilePath "pwsh.exe" -WorkingDirectory "$PSScriptRoot\..\output\$Module" -ArgumentList $ArgList
